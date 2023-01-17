653c78b002c61657a5dd0e35ee329aff047bc4ab
Vaidehi-mali <87641026+Vaidehi-mali@users.noreply.github.com>
2023-01-17 13:37:36 +0530

    Add files via upload

---
+++ Ac.js
@@ -0,0 +1,45 @@
+img="";
+status="";
+objects= [];
+
+function preload(){
+    img = loadImage('AC.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640, 220);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("MOdel Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(img, 0, 0, 640, 220);
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x + 14, objects[i].y + 15);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x - 14, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
